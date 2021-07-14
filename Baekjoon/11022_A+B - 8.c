e0157bdeb4bb0acf2534b4fa14b03d97710d0892
S4nso <71062855+S4nso@users.noreply.github.com>
2021-07-14 22:22:03 +0900

    Create 11022_A+B - 8.c

---
+++ Baekjoon/11022_A+B - 8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a,b;
+    
+    scanf("%d",&n);
+    
+    for(int i=1; i<=n; i++)
+    {
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+        
+        
+    }
+
+    return 0;
+}
+
+/**이번에는 c*/
