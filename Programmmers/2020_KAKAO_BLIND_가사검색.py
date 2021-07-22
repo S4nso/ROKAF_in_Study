5fe0def8bca93af793822875f445c525ea442ae7
S4nso <71062855+S4nso@users.noreply.github.com>
2021-07-22 23:11:51 +0900

    Update 2020_KAKAO_BLIND_가사검색.py

--- Programmmers/2020_KAKAO_BLIND_가사검색.py
+++ Programmmers/2020_KAKAO_BLIND_가사검색.py
@@ -27,6 +27,7 @@ print(solution(words,queries))
 
 Trie 트라이 자료구조 X 효율성 X'''
 
+# https://velog.io/@gojaegaebal/210126-%EA%B0%9C%EB%B0%9C%EC%9D%BC%EC%A7%8050%EC%9D%BC%EC%B0%A8-%ED%8A%B8%EB%9D%BC%EC%9D%B4Trie-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B0%9C%EB%85%90-%EB%B0%8F-%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%97%90%EC%84%9C-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0feat.-Class
 
 class Node:
     def __init__(self, data):
