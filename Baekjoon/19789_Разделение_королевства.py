4da2ec67c6ea7b2b3907d3e7297b37e1d448e93e
S4nso <71062855+S4nso@users.noreply.github.com>
2021-07-23 23:20:04 +0900

    Create 19789_Разделение_королевства.py

---
+++ Baekjoon/19789_Разделение_королевства.py
@@ -0,0 +1,24 @@
+#! /bin/usr/python3
+
+n = int(input())
+ 
+a = list()
+b = list()
+x = set()
+y = set()
+for i in range(n):
+    x1, y1 = map(int, input().split())
+    a.append([x1, y1])
+    b.append([y1, x1])
+a.sort()
+b.sort()
+for i in range (n - 1):
+    if a[i+1][0] == a[i][0] and a[i+1][1] > a[i][1]:
+        y.add(a[i][1] + 1)
+    else:
+        x.add(a[i][0] + 1)
+print(len(x) + len(y))
+for i in x:
+    print("x " + str(i))
+for i in y:
+    print("y " + str(i))
