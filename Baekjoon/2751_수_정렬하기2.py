86b32ea99bd15021a187cb6162c2b036d960ab68
S4nso <71062855+S4nso@users.noreply.github.com>
2021-07-23 23:05:10 +0900

    Update 2751_수_정렬하기2.py

--- Baekjoon/2751_수_정렬하기2.py
+++ Baekjoon/2751_수_정렬하기2.py
@@ -10,3 +10,14 @@ for i in range(a):
     
 
 # 시간초과
+
+
+
+
+import sys
+n = int(input())
+l = []
+for i in range(n):
+    l.append(int(sys.stdin.readline()))
+for i in sorted(l):
+    sys.stdout.write(str(i)+'\n')
