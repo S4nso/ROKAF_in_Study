0265bc130c15bb6ce9f9f0ebce21c93759baa02a
S4nso <71062855+S4nso@users.noreply.github.com>
2022-01-04 07:42:54 +0900

    Create 1019.cpp

---
+++ C++/codeup/1019.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main(){
+    
+    int y,m,d;
+    
+    scanf("%d.%d.%d",&y,&m,&d);
+    printf("%04d.%02d.%02d",y,m,d);
+    
+    return 0;
+}
