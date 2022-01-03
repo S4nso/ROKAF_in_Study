66a7a01422f3b36df4efc308e271d7d0505edd58
S4nso <71062855+S4nso@users.noreply.github.com>
2022-01-04 07:44:43 +0900

    Create 1020.cpp

---
+++ C++/codeup/1020.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main(){
+    
+    int a,b;
+    
+    scanf("%d-%d",&a,&b);
+    printf("%06d%07d",a,b);
+    
+    return 0;
+}
