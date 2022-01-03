53d53a16c02814a9641e041edbde534d4c64770f
S4nso <71062855+S4nso@users.noreply.github.com>
2022-01-04 07:35:22 +0900

    Create 1015.cpp

---
+++ C++/codeup/1015.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main(){
+    
+    float f;
+    
+    cin >> f;
+    cout << fixed << setprecision(2) << f;
+
+    return 0;
+}
